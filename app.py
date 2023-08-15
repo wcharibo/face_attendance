a0e1e9b7d091006f0c30c40a9645dc0eac32cb24
wcharibo <woonggi4283@gmail.com>
2023-08-16 00:42:53 +0900

    Add function to upload image

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
-if __name__=='__main__':
-    app.run()
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def predict():
+    imagefile = request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
