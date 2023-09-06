eba8f1c1f09dbd7b6ebfc0c92c0358fbd2d2c80c
wcharibo <woonggi4283@gmail.com>
2023-09-06 14:11:26 +0900

    Add students, lecture table

--- faceAttendance/models.py
+++ faceAttendance/models.py
@@ -12,3 +12,24 @@ class Answer(db.Model):
     question = db.relationship('Question', backref=db.backref('answer_set', ))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+# 학생 테이블
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    image_path = db.Column(db.String(255), nullable=False)  # 이미지 경로를 저장할 수 있는 열
+
+# 수업 테이블
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String(200), nullable=False)
+    image_path = db.Column(db.String(255), nullable=False)  # 이미지 경로를 저장할 수 있는 열
+
+# 수업-학생 매핑 테이블
+class CourseStudent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+
+    course = db.relationship('Course', backref=db.backref('students', lazy=True))
+    student = db.relationship('Student', backref=db.backref('courses', lazy=True))
--- faceAttendance/views/main_views.py
+++ faceAttendance/views/main_views.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template , request
 
+from faceAttendance.models import Student
+
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def hello_world():
-    return render_template('index.html')
+    student_list = Student.query.order_by(Student.id.desc())
+    return render_template('index.html', student_list = student_list)
 
 
 @bp.route('/', methods=['POST'])
@@ -14,3 +17,8 @@ def predict():
     imagefile.save(image_path)
 
     return render_template('index.html')
+
+@bp.route('/detail/<int:student_id>/', methods=['GET'])
+def hello_student(student_id):
+    student = Student.query.get(student_id)
+    return render_template('student.html', student = student)
