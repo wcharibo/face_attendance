543fc84df03dd5845e7f05e5841a84918391aebb
wcharibo <woonggi4283@gmail.com>
2023-09-10 05:06:22 +0900

    Add modules for face attendance and model

--- faceAttendance/models.py
+++ faceAttendance/models.py
@@ -9,9 +9,10 @@ class Question(db.Model):
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
-    question = db.relationship('Question', backref=db.backref('answer_set', ))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+    question = db.relationship('Question', backref=db.backref('answer_set', ))
 
 # 학생 테이블
 class Student(db.Model):
@@ -33,3 +34,19 @@ class CourseStudent(db.Model):
 
     course = db.relationship('Course', backref=db.backref('students', lazy=True))
     student = db.relationship('Student', backref=db.backref('courses', lazy=True))
+
+# 학생과 출결 체크 간의 관계를 다루는 연결 테이블
+student_attendance = db.Table(
+    'student_attendance',
+    db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
+    db.Column('attendance_check_id', db.Integer, db.ForeignKey('attendance_check.id'), primary_key=True),
+)
+
+class AttendanceCheck(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    check_date = db.Column(db.Date, nullable=False)
+    result = db.Column(db.Boolean, nullable=False)
+
+    course = db.relationship('Course', backref=db.backref('attendance_checks', lazy=True))
+    students = db.relationship('Student', secondary=student_attendance, lazy='subquery', backref=db.backref('attendance_checks', lazy=True))
--- faceAttendance/views/main_views.py
+++ faceAttendance/views/main_views.py
@@ -1,13 +1,23 @@
 from flask import Blueprint, render_template , request
+from faceAttendance.models import Student, Course, CourseStudent
 
-from faceAttendance.models import Student
+#for AI
+import numpy as np
+import cv2
+import os
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from skimage.transform import resize
+from keras.models import load_model
+from retinaface import RetinaFace
+
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def hello_world():
-    student_list = Student.query.order_by(Student.id.desc())
-    return render_template('index.html', student_list = student_list)
+    course_list = Course.query.order_by(Course.id)
+    return render_template('index.html', course_list = course_list)
 
 
 @bp.route('/', methods=['POST'])
@@ -22,3 +32,11 @@ def predict():
 def hello_student(student_id):
     student = Student.query.get(student_id)
     return render_template('student.html', student = student)
+
+@bp.route('/', methods=['POST'])
+def train():
+    imagefile = request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    return render_template('index.html')
