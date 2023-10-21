85af4434c47f0f2bb5f58832249819c197a10342
wcharibo <woonggi4283@gmail.com>
2023-10-22 00:19:48 +0900

    Add week select btn, include face check result in DB

--- faceAttendance/views/main_views.py
+++ faceAttendance/views/main_views.py
@@ -5,13 +5,13 @@ from faceAttendance import db
 
 #for AI
 import os
-# import numpy as np
-# import cv2
-# from sklearn.svm import SVC
-# from sklearn.preprocessing import LabelEncoder
-# from skimage.transform import resize
-# from keras.models import load_model
-# from retinaface import RetinaFace
+import numpy as np
+import cv2
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from skimage.transform import resize
+from keras.models import load_model
+from retinaface import RetinaFace
 
 
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -74,157 +74,181 @@ def detail(course_id):
     else:
         flash('로그인 후 사용해주세요')
         return redirect(url_for('auth.login'))
-    # model_path = '../static/model/keras/model/facenet_keras.h5'
-    # global model
-    # model = load_model(model_path)
+    model_path = '../static/model/keras/model/facenet_keras.h5'
+    global model
+    model = load_model(model_path)
     return render_template('course.html', course = course)
 
-# @bp.route('/detail/<int:course_id>/', methods=['POST'])
-# def predict(course_id):
-#     global course
-#     course = Course.query.get(course_id)
+@bp.route('/detail/<int:course_id>/', methods=['POST'])
+def predict(course_id):
+    global course
+    course = Course.query.get(course_id)
 
-#     imagefiles = request.files.getlist('imagefile')
+    imagefiles = request.files.getlist('imagefile')
+    week_number = int(request.form['week'])
 
-#     for imagefile in imagefiles:
-#         if imagefile.filename !='':
-#             test_image_path = '../static/images/lectures/' + course.course_name + '/' + imagefile.filename
-#             imagefile.save(test_image_path)
+    for imagefile in imagefiles:
+        if imagefile.filename !='':
+            test_image_path = '../static/images/lectures/' + course.course_name + '/' + imagefile.filename
+            imagefile.save(test_image_path)
 
 
-#     global image_size
-#     image_size=160
+    global image_size
+    image_size=160
 
-#     embs, labels, names = check_trained(course)
-#     le, clf = test(embs, labels )
+    embs, labels, names = check_trained(course)
+    le, clf = test(embs, labels )
 
-#     test_dirpath = '../static/images/lectures/' + course.course_name + '/'
-#     test_filepaths = [os.path.join(test_dirpath, f) for f in os.listdir(test_dirpath)]
+    test_dirpath = '../static/images/lectures/' + course.course_name + '/'
+    test_filepaths = [os.path.join(test_dirpath, f) for f in os.listdir(test_dirpath)]
 
-#     pred, pred_proba = infer(le, clf, test_filepaths)
-#     result = list(np.unique(pred))
+    pred, pred_proba = infer(le, clf, test_filepaths)
+    result = list(np.unique(pred))
 
-#     for test_image_path in test_filepaths:
-#         os.remove(test_image_path)
+    for student_id in result:
+        # 해당 주차의 출석 정보가 이미 DB에 있는지 확인
+        attendance_check = AttendanceCheck.query.filter_by(
+            course_id=course.id,
+            student_id=student_id,
+            check_week=week_number  # 어떤 주차의 출석을 업데이트할지 지정
+        ).first()
 
-#     return render_template('course.html', course = course, id=course_id_to_query, result = result, len_result=len(result), total_students=len(names))
+        # 만약 해당 주차의 출석 정보가 없으면 새로운 레코드를 생성
+        # if attendance_check is None:
+        #     attendance_check = AttendanceCheck(
+        #         course_id=course.id,
+        #         student_id=student_id,
+        #         check_week=week_number,
+        #         result=True  # 얼굴 인식 결과가 True 또는 False인지에 따라 저장할 값 지정
+        #     )
+        #     db.session.add(attendance_check)
+        # else:
+            # 이미 해당 주차의 출석 정보가 있는 경우, 결과를 업데이트
+        attendance_check.result = True  # 얼굴 인식 결과가 True 또는 False인지에 따라 업데이트할 값 지정
 
-# # def check_trained(course):
-#     students_in_course = Student.query.join(CourseStudent, (CourseStudent.student_id == Student.id)).filter(CourseStudent.course_id == course.id).all()
-#     names = [student.id for student in students_in_course]
+    db.session.commit()  # 변경사항을 DB에 저장
 
-#     if os.path.isfile("../static/lectures/" + course.course_name +"/embedding/np_embs.npy"):
-#         embs=np.load('../static/lectures/'+course.course_name+'/embedding/np_embs.npy')
-#         labels = []
-#         with open('../static/lectures/'+course.course_name+'/embedding/labels.txt' , 'r') as file:
-#             for line in file:
-#                 labels.append(line.strip())
+    for test_image_path in test_filepaths:
+        os.remove(test_image_path)
 
-#         return embs, labels, names
-#     else:
-#         image_dir_basepath= '../static/lectures/' + course.course_name + '/images/'
-#         embs, labels = train(image_dir_basepath, names)
-#         return embs, labels, names
+    return render_template('course.html', course = course, result = result, len_result=len(result), total_students=len(names))
 
-# def prewhiten(x):
-#     #print('prewhiten - x shape: ', x.shape)
-#     if x.ndim == 4:
-#         axis = (1, 2, 3)
-#         size = x[0].size
-#     elif x.ndim == 3:
-#         axis = (0, 1, 2)
-#         size = x.size
-#     else:
-#         raise ValueError('Dimension should be 3 or 4')
+def check_trained(course):
+    students_in_course = Student.query.join(CourseStudent, (CourseStudent.student_id == Student.id)).filter(CourseStudent.course_id == course.id).all()
+    names = [student.id for student in students_in_course]
 
-#     mean = np.mean(x, axis=axis, keepdims=True)
-#     std = np.std(x, axis=axis, keepdims=True)
-#     std_adj = np.maximum(std, 1.0/np.sqrt(size))
-#     y = (x - mean) / std_adj
-#     return y
+    if os.path.isfile("../static/lectures/" + course.course_name +"/embedding/np_embs.npy"):
+        embs=np.load('../static/lectures/'+course.course_name+'/embedding/np_embs.npy')
+        labels = []
+        with open('../static/lectures/'+course.course_name+'/embedding/labels.txt' , 'r') as file:
+            for line in file:
+                labels.append(line.strip())
 
-# def l2_normalize(x, axis=-1, epsilon=1e-10):
-#     output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
-#     return output
+        return embs, labels, names
+    else:
+        image_dir_basepath= '../static/lectures/' + course.course_name + '/images/'
+        embs, labels = train(image_dir_basepath, names)
+        return embs, labels, names
 
-# def train_load_and_align_images(filepaths, margin):
+def prewhiten(x):
+    #print('prewhiten - x shape: ', x.shape)
+    if x.ndim == 4:
+        axis = (1, 2, 3)
+        size = x[0].size
+    elif x.ndim == 3:
+        axis = (0, 1, 2)
+        size = x.size
+    else:
+        raise ValueError('Dimension should be 3 or 4')
 
-#     aligned_images = []
-#     for filepath in filepaths:
-#         img = cv2.imread(filepath)
-#         faces = RetinaFace.extract_faces(img, align = True)
-#         for face in faces:
-#             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-#             aligned = resize(face, (image_size, image_size), anti_aliasing=True)
-#             aligned_images.append(aligned)
-#     global var_faces
-#     var_faces = np.array(aligned_images)
+    mean = np.mean(x, axis=axis, keepdims=True)
+    std = np.std(x, axis=axis, keepdims=True)
+    std_adj = np.maximum(std, 1.0/np.sqrt(size))
+    y = (x - mean) / std_adj
+    return y
 
-#     return np.array(aligned_images)
+def l2_normalize(x, axis=-1, epsilon=1e-10):
+    output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
+    return output
 
-# def test_load_and_align_images(filepaths, margin):
+def train_load_and_align_images(filepaths, margin):
 
-#     aligned_images = []
-#     for filepath in filepaths:
-#         img = cv2.imread(filepath)
-#         faces = RetinaFace.extract_faces(img, align = True)
-#         for face in faces:
-#             if(face.size<20000):
-#                 continue
-#             face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-#             aligned = resize(face, (image_size, image_size), anti_aliasing=True)
-#             aligned_images.append(aligned)
-#     global var_faces
-#     var_faces = np.array(aligned_images)
+    aligned_images = []
+    for filepath in filepaths:
+        img = cv2.imread(filepath)
+        faces = RetinaFace.extract_faces(img, align = True)
+        for face in faces:
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            aligned = resize(face, (image_size, image_size), anti_aliasing=True)
+            aligned_images.append(aligned)
+    global var_faces
+    var_faces = np.array(aligned_images)
 
-#     return np.array(aligned_images)
+    return np.array(aligned_images)
 
-# def train_calc_embs(filepaths, margin=10, batch_size=1):
-#     aligned_images = prewhiten(train_load_and_align_images(filepaths, margin))
-#     pd = []
-#     for start in range(0, len(aligned_images), batch_size):
-#         pd.append(model.predict_on_batch(aligned_images[start:start+batch_size]))
-#     embs = l2_normalize(np.concatenate(pd))
-#     #print(embs.shape)
+def test_load_and_align_images(filepaths, margin):
 
-#     return embs
+    aligned_images = []
+    for filepath in filepaths:
+        img = cv2.imread(filepath)
+        faces = RetinaFace.extract_faces(img, align = True)
+        for face in faces:
+            if(face.size<20000):
+                continue
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            aligned = resize(face, (image_size, image_size), anti_aliasing=True)
+            aligned_images.append(aligned)
+    global var_faces
+    var_faces = np.array(aligned_images)
 
-# def test_calc_embs(filepaths, margin=10, batch_size=1):
-#     aligned_images = prewhiten(test_load_and_align_images(filepaths, margin))
-#     pd = []
-#     for start in range(0, len(aligned_images), batch_size):
-#         pd.append(model.predict_on_batch(aligned_images[start:start+batch_size]))
-#     embs = l2_normalize(np.concatenate(pd))
-#     #print(embs.shape)
+    return np.array(aligned_images)
 
-#     return embs
+def train_calc_embs(filepaths, margin=10, batch_size=1):
+    aligned_images = prewhiten(train_load_and_align_images(filepaths, margin))
+    pd = []
+    for start in range(0, len(aligned_images), batch_size):
+        pd.append(model.predict_on_batch(aligned_images[start:start+batch_size]))
+    embs = l2_normalize(np.concatenate(pd))
+    #print(embs.shape)
 
-# def train(dir_basepath, names, max_num_img=10):
-#     labels = []
-#     embs = []
-#     for name in names:
-#         dirpath = os.path.abspath(dir_basepath + str(name))
-#         filepaths = [os.path.join(dirpath, f) for f in os.listdir(dirpath)][:max_num_img]
-#         embs_ = train_calc_embs(filepaths)
-#         labels.extend([name] * len(embs_))
-#         embs.append(embs_)
-#         #print(name)
+    return embs
 
-#     embs = np.concatenate(embs)
-#     np.save('../static/lectures/'+course.course_name+'/embedding/np_embs.npy', embs)
-#     with open('../static/lectures/'+course.course_name+'/embedding/labels.txt', 'w') as file:
-#         for item in labels:
-#             file.write(f'{item}\n')
-#     return embs, labels
+def test_calc_embs(filepaths, margin=10, batch_size=1):
+    aligned_images = prewhiten(test_load_and_align_images(filepaths, margin))
+    pd = []
+    for start in range(0, len(aligned_images), batch_size):
+        pd.append(model.predict_on_batch(aligned_images[start:start+batch_size]))
+    embs = l2_normalize(np.concatenate(pd))
+    #print(embs.shape)
 
-# def test(embs, labels):
-#     le = LabelEncoder().fit(labels)
-#     y= le.transform(labels)
-#     clf = clf = SVC(kernel='linear', probability=True).fit(embs, y)
-#     return le, clf
+    return embs
 
-# def infer(le, clf, filepaths):
-#     embs = test_calc_embs(filepaths)
-#     pred = le.inverse_transform(clf.predict(embs))
-#     pred_proba=clf.predict_proba(embs)
-#     return pred, pred_proba
+def train(dir_basepath, names, max_num_img=10):
+    labels = []
+    embs = []
+    for name in names:
+        dirpath = os.path.abspath(dir_basepath + str(name))
+        filepaths = [os.path.join(dirpath, f) for f in os.listdir(dirpath)][:max_num_img]
+        embs_ = train_calc_embs(filepaths)
+        labels.extend([name] * len(embs_))
+        embs.append(embs_)
+        #print(name)
+
+    embs = np.concatenate(embs)
+    np.save('../static/lectures/'+course.course_name+'/embedding/np_embs.npy', embs)
+    with open('../static/lectures/'+course.course_name+'/embedding/labels.txt', 'w') as file:
+        for item in labels:
+            file.write(f'{item}\n')
+    return embs, labels
+
+def test(embs, labels):
+    le = LabelEncoder().fit(labels)
+    y= le.transform(labels)
+    clf = clf = SVC(kernel='linear', probability=True).fit(embs, y)
+    return le, clf
+
+def infer(le, clf, filepaths):
+    embs = test_calc_embs(filepaths)
+    pred = le.inverse_transform(clf.predict(embs))
+    pred_proba=clf.predict_proba(embs)
+    return pred, pred_proba
